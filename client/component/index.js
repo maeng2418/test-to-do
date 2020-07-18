fab6af839c8d74a37eb978bdaaa7be8d9f536bfd
MyeongSeong <maeng2418@naver.com>
2020-07-18 10:12:43 +0900

    Component 클래스 생성
    - 컴포넌트 클래스 제작
    - 생성자를 활용해서 부모 객체 받아옴.
    - 생성자를 활용해서 props를 넣어줌.
    - 생성자를 활용해서 className을 가진 외부 엘리먼트(div) 생성.
    - mount() 이용해서 부모 element에 삽입.
    - render() 이용해서 내부 엘리먼트(div) 생성.

---
+++ client/component/index.js
@@ -0,0 +1,22 @@
+export default class Component {
+    constructor(parent = null, props, className) {
+        // 부모 객체 가져옴.
+        this.parent = parent;
+        // props 가져옴.
+        this.props = props;
+        // div 생성
+        this.$ = document.createElement('div');
+        this.$.classList.add(className);
+        this.$.innerHTML = this.render();
+    }
+
+    // 부모 컴포넌트에 집어넣기
+    mount(parentElement) {
+        parentElement.append(this.$);
+    }
+
+    // 컴포넌트 생성함수
+    render() {
+        return '';
+    }
+}
